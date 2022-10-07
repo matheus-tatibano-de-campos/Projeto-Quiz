896c8283b412ca20e98efeabf5f47d1d7a8207a7
matheus tatibano <matheus.tatibano@gmail.com>
2022-10-07 15:45:32 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'https://exercicios-curso-next-1htc-matheus-tatibano-de-campos.vercel.app/'
+const BASE_URL = 'https://exercicios-curso-next-1htc-21c05n45q-matheus-tatibano-de-campos.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
